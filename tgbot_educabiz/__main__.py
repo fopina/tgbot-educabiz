6956775b17c40d0a166d96b5168e1e4761828e77
Filipe Pina <hzlu1ot0@duck.com>
2024-10-24 06:23:29 +0100

    x

--- tgbot_educabiz/__main__.py
+++ tgbot_educabiz/__main__.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env -S python3 -u
 
 import logging
-import os
-import uuid
 
-import dotenv
-from telegram import Update
-
-from .bot import setup_app
-
-dotenv.load_dotenv()
+from .bot import Bot
+from .env import env
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -21,21 +15,12 @@ logger = logging.getLogger(__name__)
 
 def main() -> None:
     """Start the bot."""
-    application = setup_app(os.getenv('TGEB_TOKEN'))
-
-    # Run the bot until the user presses Ctrl-C
-    webhook_url = os.getenv('TGEB_WEBHOOK_URL')
-    if webhook_url:
-        # TODO: check with upstream if random secret_token should not be handled BY DEFAULT
-        secure_uuid = uuid.uuid4()
-        application.run_webhook(
-            port=os.getenv('TGEB_WEBHOOK_PORT', 9999),
-            webhook_url=webhook_url,
-            secret_token=secure_uuid,
-            allowed_updates=Update.ALL_TYPES,
-        )
-    else:
-        application.run_polling(allowed_updates=Update.ALL_TYPES)
+    bot = Bot(
+        env('TGEB_TOKEN'),
+        webhook_port=env('TGEB_WEBHOOK_PORT', 9999),
+        webhook_url=env('TGEB_WEBHOOK_URL'),
+    )
+    bot.run()
 
 
 if __name__ == '__main__':
--- tgbot_educabiz/bot.py
+++ tgbot_educabiz/bot.py
@@ -1,39 +1,59 @@
 #!/usr/bin/env -S python3 -u
 
 
+import uuid
+
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf'Hi {user.mention_html()}!',
-        reply_markup=ForceReply(selective=True),
-    )
+class Bot:
+    def __init__(self, token: str, webhook_url: str = None, webhook_port: int = None):
+        self._token = token
+        self._webhook_url = webhook_url
+        self._webhook_port = webhook_port
+        self._secret_token = uuid.uuid4()
 
+    # Define a few command handlers. These usually take the two arguments update and
+    # context.
+    async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Send a message when the command /start is issued."""
+        user = update.effective_user
+        await update.message.reply_html(
+            rf'Hi {user.mention_html()}!',
+            reply_markup=ForceReply(selective=True),
+        )
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text('Help!')
+    async def help_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Send a message when the command /help is issued."""
+        await update.message.reply_text('Help!')
 
+    async def echo(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Echo the user message."""
+        await update.message.reply_text(update.message.text)
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
+    def setup_app(self) -> Application:
+        # Create the Application and pass it your bot's token.
+        application = Application.builder().token(self._token).build()
 
+        # on different commands - answer in Telegram
+        application.add_handler(CommandHandler('start', self.start))
+        application.add_handler(CommandHandler('help', self.help_command))
 
-def setup_app(token: str) -> Application:
-    # Create the Application and pass it your bot's token.
-    application = Application.builder().token(token).build()
+        # on non command i.e message - echo the message on Telegram
+        application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.echo))
+        return application
 
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('help', help_command))
-
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-    return application
+    def run(self):
+        application = self.setup_app()
+        # Run the bot until the user presses Ctrl-C
+        if self._webhook_url:
+            # TODO: check with upstream if random secret_token should not be handled BY DEFAULT
+            application.run_webhook(
+                port=self._webhook_port,
+                webhook_url=self._webhook_url,
+                secret_token=self._secret_token,
+                allowed_updates=Update.ALL_TYPES,
+            )
+        else:
+            application.run_polling(allowed_updates=Update.ALL_TYPES)
